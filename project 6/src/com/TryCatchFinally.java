e21c6cbaff5d2c1002ab5c808ee7b9b70a7624bb
Admin <Admin@192.168.0.101>
2022-06-29 20:59:39 -0700

    project

---
+++ project 6/src/com/TryCatchFinally.java
@@ -0,0 +1,23 @@
+package com;
+
+public class TryCatchFinally {
+
+	public static void main(String[] args) {
+		int[] arr = new int[4];
+		try
+		{
+			int i = arr[4];
+			System.out.println("Inside try block");
+		}
+catch(ArrayIndexOutOfBoundsException ex)
+		{
+	System.out.println("Exception caught in catch block");
+		}
+		finally
+		{
+			System.out.println("finally block executed");
+		}
+		System.out.println("Outside try-catch-finally clause");
+	}
+
+}
